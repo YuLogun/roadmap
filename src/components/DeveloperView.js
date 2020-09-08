16c82d350c4c03f9ef47f11bd09e2679cf7fb7ce
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-09-08 20:43:06 +0400

    first commit

---
+++ src/components/DeveloperView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeveloperView = () => {
+  return <h1>DeveloperView</h1>;
+};
+
+export default DeveloperView;
---
+++ src/components/ManagerView.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ManagerView = () => {
+  return (
+    <div>
+      <h1>ManagerView</h1>
+    </div>
+  );
+};
+
+export default ManagerView;

2ec255863cab218f04eba2779ba17c48b3527061
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-09-12 11:42:57 +0400

    added .prettierrc config for prettier code formatter

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,12 @@
-import { coursesTestData } from "../components/DeveloperView/coursesTestData";
+import { coursesTestData } from '../components/DeveloperView/coursesTestData';
 
 const initialState = {};
 
-export const getDataAction = { type: "GET_DATA" };
+export const getDataAction = { type: 'GET_DATA' };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case "GET_DATA":
+    case 'GET_DATA':
       return { ...state, coursesTestData };
     default:
       return state;

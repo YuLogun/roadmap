87892efcda6c5c46da1b2f99841ed659e7b5f9b6
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-09-18 17:04:05 +0400

    updated css

--- src/components/Roadmap/Roadmap.js
+++ src/components/Roadmap/Roadmap.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from 'react';
 
 //components
-import Technology from "../Technology/Technology";
-import { Typography } from "@material-ui/core";
+import Technology from '../Technology/Technology';
+import { Typography } from '@material-ui/core';
 
 //styles
 import { useStyles } from './Roadmap.styles';
@@ -12,24 +12,25 @@ const Roadmap = ({ roadmapTitle, coursesTestData, managerView, styles }) => {
 
   return (
     <div className={classes.container}>
-    {/* <div className={`container ${styles}`}> */}
-      <ul className={classes.roadmapTitle}>
-      {/* <ul className="list"> */}
-        <li>
-          <Typography variant="h1">{roadmapTitle}</Typography>
-          <ul className={classes.technologyTitle}>
-            {coursesTestData.map((it) => (
-              <Technology
-                key={it.id}
-                techTitle={it.technology}
-                courses={it.courses}
-                id={it.id}
-                managerView={managerView}
-              />
-            ))}
-          </ul>
-        </li>
-      </ul>
+      <div className={`container ${styles}`}>
+        <ul className={classes.roadmapTitle}>
+          {/* <ul className="list"> */}
+          <li>
+            <Typography variant="h1">{roadmapTitle}</Typography>
+            <ul className={classes.technologyTitle}>
+              {coursesTestData.map((it) => (
+                <Technology
+                  key={it.id}
+                  techTitle={it.technology}
+                  courses={it.courses}
+                  id={it.id}
+                  managerView={managerView}
+                />
+              ))}
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };

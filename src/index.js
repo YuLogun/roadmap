8402fe5e5bba6717377448dc606d74e013b1175f
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-09-23 16:05:38 +0400

    changed router to be hashRouter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 //materialUI
-import { ThemeProvider } from "@material-ui/styles";
-import Theme from "./theme/theme";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from '@material-ui/styles';
+import Theme from './theme/theme';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 //redux
-import { Provider } from "react-redux";
-import store from "./redux/store";
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <ThemeProvider theme={Theme}>
           <CssBaseline />
           <App />
         </ThemeProvider>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
